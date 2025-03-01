43ff0a83ba80602c7ab2c9b4f3cb12f2da2bde50
nishu1206 <41357761+nishu1206@users.noreply.github.com>
2025-03-01 22:53:19 +0530

    Update index.js with new component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Website from './website/Website'; 
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    // <App />
+    <Website />
   </React.StrictMode>,
   document.getElementById('root')
 );
